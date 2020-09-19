c5eb41023abd929d4366212f14b0eb1fcd7a081e
socratone <gim2origin@gmail.com>
2020-09-19 10:39:22 +0900

    주석 추가

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
+    // 배열의 순서대로 적용된다.
     rules: [
       {
         loader: 'babel-loader',
+        test: /\.jsx?$/, // jsx만 컴파일한다.
       },
     ],
   },
