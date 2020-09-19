d5bf0ea5205a3e7493c476d440f6419dc67845dc
socratone <gim2origin@gmail.com>
2020-09-19 21:51:13 +0900

    개발 서버 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'none',
-  entry: './src/index.js', // import된 파일들도 불러온다.
+  entry: './src/index.js', // index.js부터 시작해서 여기에 import된 모든 파일이 bundle.js에 담긴다.
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'), // dist 폴더에 build된다.
   },
   module: {
-    // 배열의 순서대로 적용된다.
     rules: [
       {
         test: /\.js$/, // .js 확장자로 끝나는 파일만 babel-loader를 사용한다.
@@ -17,17 +17,20 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        // css-loader가 css를 가져온다.
-        // style-loader가 html head에 style 태그를 넣어준다.
+        // 먼저 css-loader가 css를 가져온다.
+        // 다음으로 style-loader가 html head에 style 태그를 넣어준다.
         use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html', // src/index.html 파일을 읽어서
+      filename: 'index.html', // output으로 설정한 bundle.js를 추가하고 dist/index.html 파일을 만든다.
+    }),
+  ],
   devServer: {
-    // 개발 서버를 띄울 때 어느 경로에 있는 것을 불러올 것인지
-    contentBase: path.join(__dirname, 'public'),
-    // 개발 서버에서 번들된 js파일을 어느 경로에서 불러올 것인지
-    // 위와 같은 경로로 설정하면 충돌이 생기는 것 같다.
-    publicPath: '/dist/',
+    // 개발 서버를 띄우면 dist/index.html을 열고 index.html에서 불러오는 bundle.js를 임시로 컴파일 해준다.
+    contentBase: path.join(__dirname, 'dist'), // dist의 index.html을 불러온다.
   },
 };
